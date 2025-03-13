205e89b40d2a6db1cb2e6edfffce33dfeae982c7
UncleChair <voidgun1998@gmail.com>
2025-03-13 20:44:25 +0800

    Refactor: move observer related interface to individual package

---
+++ C2/observer/observer.go
@@ -0,0 +1,15 @@
+package observer
+
+type Subject interface {
+	RegisterObserver(observer Observer)
+	RemoveObserver(observer Observer)
+	NotifyObservers()
+}
+
+type Observer interface {
+	Update(temp float32, humidity float32, pressure float32)
+}
+
+type DisplayElement interface {
+	Display()
+}
--- C2/weather/weather.go
+++ C2/weather/weather.go
@@ -1,19 +1,5 @@
 package weather
 
-type Subject interface {
-	RegisterObserver(observer Observer)
-	RemoveObserver(observer Observer)
-	NotifyObservers()
-}
-
-type Observer interface {
-	Update(temp float32, humidity float32, pressure float32)
-}
-
-type DisplayElement interface {
-	Display()
-}
-
 func WeatherImplements() {
 	// TODO
 }
